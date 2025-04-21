fccbb1ff39469a5301b7b381630e322556413e37
Kazi Sadib Reza <kazisadibreza@gmail.com>
2025-04-22 02:46:17 +0600

    add image to checkout

--- wp-content/plugins/logo-to-image-generator/includes/cart-image-handler.php
+++ wp-content/plugins/logo-to-image-generator/includes/cart-image-handler.php
@@ -21,8 +21,14 @@ class LIG_Cart_Image_Handler {
         // Set the selected image as the cart item image
         add_filter('woocommerce_cart_item_thumbnail', array($this, 'change_cart_item_thumbnail'), 10, 3);
         
-        // Change image in checkout review
+        // Change image in checkout review - legacy hook
         add_filter('woocommerce_in_cart_product_thumbnail', array($this, 'change_cart_item_thumbnail'), 10, 3);
+        
+        // Specific hook for checkout page thumbnails
+        add_filter('woocommerce_checkout_cart_item_thumbnail', array($this, 'change_cart_item_thumbnail'), 10, 3);
+        
+        // Filter product image globally - useful for checkout pages with different hooks
+        add_filter('woocommerce_product_get_image', array($this, 'filter_product_image'), 20, 2);
         
         // Change image in mini-cart
         add_filter('woocommerce_cart_item_thumbnail', array($this, 'change_cart_item_thumbnail'), 10, 3);
@@ -51,10 +57,7 @@ class LIG_Cart_Image_Handler {
      * Preserve custom cart item data when updating quantity
      */
     public function preserve_custom_cart_item_data($cart_item, $cart_id) {
-        if (isset($cart_item['lig_selected_image'])) {
-            // Make sure the image URL is preserved
-            $cart_item['lig_selected_image'] = $cart_item['lig_selected_image'];
-        }
+        // Simply return the cart item - no need to reassign to itself
         return $cart_item;
     }
 
@@ -69,6 +72,35 @@ class LIG_Cart_Image_Handler {
             $thumbnail = '<img src="' . esc_url($image_url) . '" class="lig-custom-thumbnail" alt="Product image" />';
         }
         return $thumbnail;
+    }
+
+    /**
+     * Filter product images globally - especially useful for checkout
+     */
+    public function filter_product_image($image, $product) {
+        // Only apply during checkout
+        if (!is_checkout()) {
+            return $image;
+        }
+        
+        // Get the cart to check for our custom images
+        $cart = WC()->cart;
+        if (!$cart) {
+            return $image;
+        }
+        
+        // Check if this product is in the cart with our custom image
+        foreach ($cart->get_cart() as $cart_item) {
+            if (isset($cart_item['lig_selected_image']) && 
+                ($cart_item['product_id'] == $product->get_id() || 
+                 (isset($cart_item['variation_id']) && $cart_item['variation_id'] == $product->get_id()))) {
+                
+                return '<img src="' . esc_url($cart_item['lig_selected_image']) . 
+                       '" class="lig-custom-thumbnail" alt="Product image" width="50" height="50" />';
+            }
+        }
+        
+        return $image;
     }
 
     /**
